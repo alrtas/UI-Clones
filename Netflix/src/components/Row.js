063c2b1cf1f447ad387078b8f24dd3eaaa49aab3
Thiago Alberto da Silva <alrrtas@gmail.com>
2021-03-10 11:39:16 -0300

    Create Netflix

---
+++ Netflix/src/components/Row.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect} from 'react'
+import axios from '../axios'
+import YouTube from "react-youtube"
+import './Row.css'
+
+const base_url = "https://image.tmdb.org/t/p/original/"
+
+
+function Row({title, fetchUrl, isLargeRow}) {
+    
+    const [movies, setMovies] = useState([])
+    const [trailerUrl, setTrailerUrl] = useState("")
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {  
+            autoPlay: 1,
+        },
+    }
+
+    const handleClick = async (movie) => {
+        if (trailerUrl) {
+          setTrailerUrl("");
+        } else {
+          let trailerurl = await axios.get(
+            `/movie/${movie.id}/videos?api_key=fb34530271b349314af0de263d16ab5a`
+          );
+          setTrailerUrl(trailerurl.data.results[0]?.key);
+        }
+      };
+    
+    useEffect(() => {
+        async function fetchData(){
+            const request = await axios.get(fetchUrl)
+            setMovies(request.data.results)
+            return request
+        }
+        fetchData()
+    },[fetchUrl])
+    console.table(movies)
+
+    return (
+        <div className='row'>
+            <h2>{title}</h2>
+            <div className= "row__posters">
+              {movies.map(movie => (
+                    <img className={`row__poster ${isLargeRow && "row__posterLarge"}`} 
+                        key={movie.id}
+                        onClick={()=>handleClick(movie)}
+                        src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
+                        alt={movie.name}
+                    />
+              ))}  
+            </div>
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+        </div>
+    )
+}
+
+export default Row
