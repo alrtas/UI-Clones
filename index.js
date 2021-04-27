929aa87d249093e23beb6979b629eb30a76e4e96
Thiago Alberto da Silva <alrrtas@gmail.com>
2021-04-27 08:21:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const project =  'made with Javascript framework react.js';
+
+var i;
+for (i = 0; i < cars.length; i++) {
+  text += cars[i] + "<br>";
+}
+
+var j = new Date()
+j = new Date(year, month, day, hours, minutes, seconds, milliseconds)
+j = new Date(milliseconds)
+j = new Date(date string)
+console.log(j)
